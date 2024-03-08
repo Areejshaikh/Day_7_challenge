997426a5736089ce7630eabe65017a672dcb9252
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-03-09 02:24:09 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// Question 19
+//program number 14 
+var invitFriends = ["Areej", "Aqib", "Shahzaib", "Sharaz"];
+console.log("Am invited  ".concat(invitFriends.length, " people in dinner"));
+// Question 20
+var places = ["Turkey", "Soudia Arab", "Thailand", "Jpan", "New yourk"];
+console.log("i like this countries ", places);
+// Question 21
+var car = {
+    type: "toyyta",
+    module: "Corolla",
+    year: 2010
+};
+console.log("the car is ".concat(car.type, " module ").concat(car.module, " and  year ").concat(car.year, " "));
---
+++ app.ts
@@ -0,0 +1,20 @@
+// Question 19
+//program number 14 
+let invitFriends : string[] = ["Areej","Aqib","Shahzaib" ,"Sharaz"];
+console.log(`Am invited  ${invitFriends.length} people in dinner`);
+
+
+// Question 20
+
+let places = ["Turkey", "Soudia Arab", "Thailand", "Jpan", "New yourk"];
+console.log("i like this countries ", places);
+
+
+// Question 21
+
+let car: { type:string ; module:string ;  year :number}={
+    type :"toyota",
+    module:"Corolla",
+    year : 2010
+}
+console.log(`The car is ${car.type} module ${car.module} and  year ${car.year} `);
